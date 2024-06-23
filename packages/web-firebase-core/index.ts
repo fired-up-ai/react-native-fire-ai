913ef7621125f33752ac89c8cf78e98b95ed8988
sukrut sathaye <sukrutssathaye@gmail.com>
2024-06-23 14:47:49 +0200

    add package index for exporting all services centrally

---
+++ packages/web-firebase-core/index.ts
@@ -0,0 +1,9 @@
+import FirebaseCore from "./src/core/FirebaseCore";
+import AbstractAuth from "./src/models/AbstractAuth";
+import AuthService from "./src/services/AuthService";
+
+module.exports = {
+    FirebaseCore,
+    AbstractAuth,
+    AuthService
+};
