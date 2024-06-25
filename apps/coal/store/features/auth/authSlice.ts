7a9908c72266c7a32dd50e72c058c8f3df8b93bc
sukrut sathaye <sukrutssathaye@gmail.com>
2024-06-26 00:32:14 +0200

    update PayloadAction types

--- apps/coal/store/features/auth/authSlice.ts
+++ apps/coal/store/features/auth/authSlice.ts
@@ -15,10 +15,10 @@ const authSlice = createSlice({
     name: 'auth',
     initialState,
     reducers: {
-        setUser(state, action: PayloadAction<User>) {
+        setUser(state, action: PayloadAction<User | null>) {
             state.user = action.payload;
         },
-        setIdToken(state, action: PayloadAction<string>) {
+        setIdToken(state, action: PayloadAction<string | null>) {
             state.idToken = action.payload;
         },
         clearAuth(state) {
