366b77fe8e4c29b2054657eface5bfdf32d460fc
sukrut sathaye <sukrutssathaye@gmail.com>
2024-06-25 16:19:37 +0200

    add and export FirebaseAdmin from index

--- packages/web-firebase-core/index.ts
+++ packages/web-firebase-core/index.ts
@@ -1,9 +1,11 @@
 import FirebaseCore from "./src/core/FirebaseCore";
 import AbstractAuth from "./src/models/AbstractAuth";
 import AuthService from "./src/services/AuthService";
+import FirebaseAdmin from "./src/core/FirebaseAdmin";
 
 module.exports = {
     FirebaseCore,
     AbstractAuth,
-    AuthService
+    AuthService,
+    FirebaseAdmin
 };
