4e4016d1e10dc03e7df944aa76d8c295daa85a77
sukrut sathaye <sukrutssathaye@gmail.com>
2024-06-25 11:40:35 +0200

    update metro config

--- apps/coal/metro.config.js
+++ apps/coal/metro.config.js
@@ -2,10 +2,18 @@
 const { getDefaultConfig } = require('expo/metro-config');
 const path = require('path');
 
-const projectRoot = path.resolve(__dirname);
+const projectRoot = __dirname;
 const repositoryRoot = path.resolve(projectRoot, '../..');
+const defaultConfig = getDefaultConfig(projectRoot);
+defaultConfig.resolver.sourceExts.push('cjs');
 
 /** @type {import('expo/metro-config').MetroConfig} */
 const config = getDefaultConfig(repositoryRoot);
+
+config.watchFolders = [ repositoryRoot ];
+config.resolver.nodeModulesPaths = [
+    path.resolve(repositoryRoot, 'node_modules'),
+    path.resolve(projectRoot, 'node_modules'),
+]
 
 module.exports = config;
